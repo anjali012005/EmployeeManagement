97f02dd6f100261e0852725dfcde9997d66ccd87
Anjali Daharwal <anjalidaharwal5@gmail.com>
2026-02-24 00:44:36 +0530

    fixed register routing

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import { useState, useContext } from "react";
 import API from "../services/api";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
 import "./Login.css";
 
@@ -9,46 +9,51 @@ function Login() {
   const { login } = useContext(AuthContext);
   const navigate = useNavigate();
 
-const handleSubmit = async (e) => {
-  e.preventDefault();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-  try {
-    const { data } = await API.post("/auth/login", form);
+    try {
+      const { data } = await API.post("/auth/login", form);
 
-    // Save token & role
-    localStorage.setItem("token", data.token);
-    localStorage.setItem("role", data.role);
+      // Save token & role
+      localStorage.setItem("token", data.token);
+      localStorage.setItem("role", data.role);
 
-    login(data.token, data.role);
+      login(data.token, data.role);
 
-    if (data.role === "Admin") {
-      navigate("/admin");
-    } else {
-      navigate("/employee");
+      if (data.role === "Admin") {
+        navigate("/admin");
+      } else {
+        navigate("/employee");
+      }
+
+    } catch (error) {
+      alert(error.response?.data?.message || "Login failed");
     }
+  };
 
-  } catch (error) {
-    alert(error.response?.data?.message || "Login failed");
-  }
-};
+  return (
+    <div className="login-container">
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit} className="login-form">
+        <input
+          placeholder="Email"
+          onChange={(e) => setForm({ ...form, email: e.target.value })}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          onChange={(e) => setForm({ ...form, password: e.target.value })}
+        />
+        <button type="submit">Login</button>
 
-return (
-  <div className="login-container">
-    <h2>Login</h2>
-    <form onSubmit={handleSubmit} className="login-form">
-      <input
-        placeholder="Email"
-        onChange={(e) => setForm({ ...form, email: e.target.value })}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        onChange={(e) => setForm({ ...form, password: e.target.value })}
-      />
-      <button type="submit">Login</button>
-    </form>
-  </div>
-);
+
+      </form>
+      <p className="login-register">
+        Don’t have an account? <Link to="/signup">Register here</Link>
+      </p>
+    </div>
+  );
 }
 
 export default Login;
