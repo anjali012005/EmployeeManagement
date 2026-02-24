be7c128ec8984316058d85fa10ba065a4f4a3cb1
ANJALI DAHARWAL <131749309+anjali012005@users.noreply.github.com>
2026-02-24 10:31:51 +0530

    Update API base URL to include '/api' path

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://employeemanagement-snph.onrender.com",
+  baseURL: "https://employeemanagement-snph.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
